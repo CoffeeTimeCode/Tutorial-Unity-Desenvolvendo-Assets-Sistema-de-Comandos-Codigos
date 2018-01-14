381d0bf2442b69a294b14350bfcad823564e7031
jsnunes <jesiel.nunes@hotmail.com>
2018-01-13 22:16:53 -0200

    #1

---
+++ Assets/Scripts/Comandos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Comandos {
+	public Dictionary<string, System.Action<string, Text>> lista = new Dictionary<string, System.Action<string, Text>>();
+
+	public Comandos(){
+		this.lista.Add ("/help", (param_comando, textos)=>{
+			textos.text = "Informações de Uso dos Comandos.\n"
+				+"/limpar -\t Limpa os textos do terminal.\n"
+				+"/sair -\t Finaliza o jogo.\n";
+		});
+
+		this.lista.Add("/limpar", (param_comando, textos)=>{
+			textos.text = "";
+		});
+
+		this.lista.Add("/sair", (param_comando, textos)=>{
+			Application.Quit();
+		});
+	}
+}
---
+++ Assets/Scripts/Terminal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Terminal : MonoBehaviour {
+	private Comandos _comandos = new Comandos();
+
+	public GameObject _terminal;
+	public InputField _comando;
+	public Text _textos;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftControl)&&Input.GetKeyDown(KeyCode.T)){
+			this._terminal.SetActive (!this._terminal.activeSelf);
+			this._comando.ActivateInputField ();
+		}
+
+		if(this._comando.isFocused && this._comando.text!="" && Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)){
+			this.executar (this._comando.text, "");	
+		}
+
+	}
+
+	public void executar(string comando, string parametros){
+		System.Action<string, Text> temp;
+
+		if(this._comandos.lista.TryGetValue(comando, out temp)){
+			temp.Invoke (parametros, this._textos);
+		}else{
+			this._textos.text = "Este comando/código não é valido.";
+		}
+	}
+}
